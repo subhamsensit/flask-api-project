68f4c113f362286be83e6b1ce7dbdd291fc10aef
Zscaler <subhamone@gmail.com>
2023-02-23 21:18:17 +0530

    new flask code

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from flask import Flask,jsonify
-import subprocess,sys,json
+import subprocess,sys,json,os
+from datetime import datetime
 import logging
 app = Flask(__name__)
+# logger code
+# setting time
+LOG_TIMESTAMP_FORMAT = "%Y-%m-%d %H-%M-%S"
+
+TIME = datetime.now().strftime(LOG_TIMESTAMP_FORMAT)
+
+# below logger will create log file db_certification.log under current directory
+# file path for logging
+current_directory=os.getcwd()
+# Zoom_ipv6.txt and Team ip folder
+# setting up log file
+log_path_certification=os.path.join(current_directory,"Log_files","flask_app.log")
+logger=logging.getLogger("zoom-trigger")
+logger.setLevel(logging.DEBUG)
+format=logging.Formatter("'%(asctime)s %(message)s")
+
+
+
+
 
 @app.route("/",methods=["GET"])
 def trigger_script():
@@ -11,12 +31,14 @@ def trigger_script():
         # reading result.json file
         result_file_path=r"C:\Users\Zscaler\Documents\backup-dr-db\MR-1993-DR-Zoom-call\result.json"
         with open(result_file_path,"r") as fh:
-            print("reading the result.json file")
+            logger.debug("reading the result.json file")
             data = json.load(fh)
-
+        logger.debug(f"Returning result to client {data}")
         return jsonify(data)
     except:
-        return "failed to run the script"
+        logger.debug("Failed to send data to client")
+        data={"zoom_call_status":False,"Status":404}
+        return jsonify(data)
 
 
 if __name__ == "__main__":
